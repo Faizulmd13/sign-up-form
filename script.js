3f55d9899875d09dcd75405323832cf9f6e7126d
Faizul <faizulmd1306@gmail.com>
2025-08-16 10:56:45 +0530

    load all inputs as variables:

---
+++ script.js
@@ -0,0 +1,6 @@
+const first = document.querySelector("#first-name");
+const second = document.querySelector("#second-name");
+const email = document.querySelector("#email");
+const pw = document.querySelector("#pw");
+const pwc = document.querySelector("#pw-c");
+const submit = document.querySelector("#submit");
